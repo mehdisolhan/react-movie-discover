401041646617279fe2d8b3e2298c69768ae79609
mehdisolhan <mehdisolhan@gmail.com>
2020-07-06 15:51:51 +0300

    Updates

---
+++ precache-manifest.c539168a2b9da4f2a7ad9e81d421071c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1dda716443e6192e0c2e8a11358943",
+    "url": "/react-movie-discover/index.html"
+  },
+  {
+    "revision": "babe62f6fe9e91222b84",
+    "url": "/react-movie-discover/static/css/main.337e62c9.chunk.css"
+  },
+  {
+    "revision": "6f49401d9a54b6b13060",
+    "url": "/react-movie-discover/static/js/2.427ec619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-discover/static/js/2.427ec619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babe62f6fe9e91222b84",
+    "url": "/react-movie-discover/static/js/main.40804cef.chunk.js"
+  },
+  {
+    "revision": "828386584e0558a64c47",
+    "url": "/react-movie-discover/static/js/runtime-main.1f6767aa.js"
+  }
+]);
